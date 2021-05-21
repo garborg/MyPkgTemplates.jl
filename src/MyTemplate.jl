ea42f0efcfbe04cc197f57bd0387ee366d5a71fa
Sean Garborg <se@n-garb.org>
2021-05-20 21:21:53 -0500

    Add flag for turning off services.

--- src/MyTemplate.jl
+++ src/MyTemplate.jl
@@ -16,8 +16,9 @@ import ..TestsInSrc
 PkgTemplate templates the way I configure 'em.
 
 Assumes use of GitHub.
-Turns on free-for-open-source services if `!is_proprietary`.
-Turns on GitHub services and marks all rights reserved by owner if `is_proprietary`.
+Turns onn GitHub services if `services`.
+Turns on free-for-open-source services if `services` and `!is_proprietary`.
+Marks all rights reserved by owner if `is_proprietary`.
 Sets up testing to look for *_test.jl files within src directory.
 """
 function template(;
@@ -26,41 +27,44 @@ function template(;
     is_proprietary::Bool,
     owner::Union{Nothing,String}=nothing,
     min_julia_version::VersionNumber=v"1.6",
+    services::Bool=true,
     platforms=(; linux=true, osx=false, windows=false, x64=true, x86=false, arm64=false),
 )
     test_arm = platforms.arm64
     @assert !is_proprietary || !test_arm
-
-    gh_platforms = (; filter(kv -> first(kv) != :arm64, pairs(platforms))...)
-
-    @info """The GitHub Actions 'Documentation' job will not trigger gh-pages deploys until you
-    (having admin rights) have manually pushed to the created 'gh-pages' branch at least once."""
 
     # TODO: "UNLICENSED" or "Proprietary"?
     license_name = is_proprietary ? "UNLICENSED" : "MIT"
 
     plugins = Any[
         Git(; manifest=!is_pkg, ssh=true),
-        Documenter{GitHubActions}(),
-        GitHubActions(; gh_platforms...),
         !License,
         FlexibleLicense(; owner, name=license_name),
         Tests(; file=TestsInSrc.TEMPLATE_PATH, project=false),
     ]
 
-    if test_arm
-        # n.b. In addition to TravisCI, DroneCI also supports arm64 (and more) and is also free for open source projects
-        push!(plugins, TravisCI(;
-            osx=false,
-            windows=false,
-            x64=false,
-            arm64=true,
-            coverage=false,
-        ))
-    end
+    if services
+        gh_platforms = (; filter(kv -> first(kv) != :arm64, pairs(platforms))...)
 
-    if !is_proprietary
-        push!(plugins, Codecov())
+        @info """The GitHub Actions 'Documentation' job will not trigger gh-pages deploys until you
+        (having admin rights) have manually pushed to the created 'gh-pages' branch at least once."""
+        
+        push!(plugins, Documenter{GitHubActions}(), GitHubActions(; gh_platforms...))
+
+        if test_arm
+            # n.b. In addition to TravisCI, DroneCI also supports arm64 (and more) and is also free for open source projects
+            push!(plugins, TravisCI(;
+                osx=false,
+                windows=false,
+                x64=false,
+                arm64=true,
+                coverage=false,
+            ))
+        end
+
+        if !is_proprietary
+            push!(plugins, Codecov())
+        end
     end
 
     Template(;
